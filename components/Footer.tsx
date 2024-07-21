d5b1ad96e17b9e66233182314c898940a315e529
Pedro Estoura <pedro.estoura@gmail.com>
2024-07-22 00:40:43 +0100

    Resolved Header Footer Issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
       {/* background grid */}
 
       <div className="flex flex-col items-center">
